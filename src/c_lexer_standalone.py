ed4af3d8a9979d2ab1690eb568e92fc82915ccdd
Arham Chopra <archop@iitk.ac.in>
2017-02-09 04:08:50 +0530

    added standalone lexer

--- src/c_lexer_standalone.py
+++ src/c_lexer_standalone.py
@@ -1,1 +1,17 @@
-import 
+from c_lexer import CLexer
+
+def error_func(err_msg, line, col):
+    print("Got an error: {} \n line: {} col: {} ".format(err_msg, line, col))
+
+def look_up_func(name):
+    return False;
+
+lexer = CLexer(error_func, None, None, look_up_func)
+lexer.build()
+while True:
+    inp = raw_input(">")
+    lexer.input(inp)
+    while True:
+        tok = lexer.token();
+        if not tok: break;
+        print(tok)
