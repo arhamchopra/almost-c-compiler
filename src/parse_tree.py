26a7aacebc0930bd99ab5f9b8a9ccdb452e900a4
Anil Kumar Gupta <ganil@iitk.ac.in>
2017-02-11 02:13:09 +0530

    " Changed c_parser.py till p_init_declarator_list_1"

--- src/parse_tree.py
+++ src/parse_tree.py
@@ -12,18 +12,29 @@ def addNodes(root, child_list):
     graph.add_node(rnode)
     for child in child_list:
         #  print(child)
-        if child[0] is None:
+        if child[0] is None and child[1] is not None:
             cnode = pd.Node("id{}".format(i),label = child[1])
             i+=1
             graph.add_node(cnode)
             #  print("CHILD IS NONE")
-        else:
+            graph.add_edge(pd.Edge(rnode,cnode))
+
+        elif child[0] is not None :
             cnode = child[0]
+            graph.add_edge(pd.Edge(rnode,cnode))
+
             #  print("CHILD IS NOT NONE")
         #  print("RNODE:{} CNODE:{}".format(rnode, cnode))
-        graph.add_edge(pd.Edge(rnode,cnode))
 
     return rnode
 
+def createTypeNode(type):
+    global i
+    global graph
+    t_node = addNodes(type,[])
+    type_node = addNodes("type_specifier", [(tnode, None)])
+    decl_spec_node = addNodes("declaration_specifier",[(type_node, None)])
+    return decl_spec_node
+    
 def saveGraph():
     graph.write_png("ParseTree.png")
